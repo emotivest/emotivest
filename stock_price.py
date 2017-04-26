cece8a578a425fdb791e96d34d31d70230aa9e11
David <dcaulfie@stetson.edu>
2017-04-26 12:13:14 -0400

    Set up prices for c3 graph

--- stock_price.py
+++ stock_price.py
@@ -8,8 +8,9 @@ from datetime import date, timedelta
 
 def get_stock_data(ticker):
 	end_date = date.today()
-	start_date = end_date - timedelta(days=6)
+	start_date = end_date - timedelta(days=14)
 	stock = web.DataReader(ticker,'yahoo', start_date, end_date)
+	print(stock)
 	stock['Percent'] = (stock['Adj Close']-stock['Adj Close'].shift(1))/stock['Adj Close']*100
 	pct_change = stock['Percent'].tolist()
 	change_list=[]
@@ -18,5 +19,16 @@ def get_stock_data(ticker):
 		change_list.append(short)
 	change_list[0] = '0'
 	name = [ticker]
-	final = name+change_list
-	return final
+	final_list = name+change_list
+	dates = stock.index.values
+	date_list = ['Date']
+	for day in dates:
+		correct = str(day)[:10]
+		date_list.append(correct)
+	stock_dict = {'stock': final_list, 'date_list': date_list}
+	return stock_dict
+
+print(get_stock_data('NFLX'))
+
+
+
