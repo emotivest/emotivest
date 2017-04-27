bab4ad80d65577636ffaca058ca26983df509a4a
David <dcaulfie@stetson.edu>
2017-04-27 11:35:58 -0400

    Added new tweets and fixed graph

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,86 +1,10 @@
 $(document).ready(function(){
 
 
-// ============================Single Stock Graph==========================================
-    // var createGraph = function(result, tick){
-    //     require.config({
-    //         baseUrl: '/js',
-    //         paths: {
-    //         d3: "http://d3js.org/d3.v3.min"
-    //           }
-    //     });
-        
-    //     require(["d3", "c3"], function(d3, c3){
-            
-    //         var data = JSON.parse(result)
-    //         // console.log(data)
-    //         var cdate = data['date_list']
-    //         cdate.unshift('Dates')
-    //         var date = data['date_list']
-    //         // console.log(cdate[0])
-    //         date.shift()
-    //         var sp = data['adj_list']
-    //         var stock = data['ticker']
-    //         var chart = c3.generate({
-    //             bindto: '.g'+tick,
-    //             data: {
-    //                 x: cdate[0],
-    //                 columns: [cdate,
-    //                     sp,
-    //                     stock
-    //                 ],
-    //             },
-    //             axis: {
-    //                 y:{
-    //                     label: {
-    //                         text:"Percent Return",
-    //                         position:'inner-middle'
-    //                     }
-    //                 },
-    //                 x: {
-    //                     type: 'timeseries',
-    //                     tick: {
-    //                         format: '%Y-%m-%d'
-    //                     }
-                   
-    //                 }
-    //             }
-    //         });
-    //     });
-    // }; 
 
-    // var chartButton = $('.chart_button');
-    // chartButton.on('click', function() {
-    //     var ticker = this.dataset.ticker
-    //     console.log(ticker)
-    //     var graph = $('.ct'+ticker);
-    //     graph.css('display', 'block');
-    //     // var ticker = $('.chart_button_div').data('ticker');
-    //     $.ajax({
-    //         method: 'GET',
-    //         url: '/chart-data/'+ticker,
-    //         // method: 'GET',
-    //         success: function(result){
-    //             var data = result
-    //             var date = result['date_list']
-    //             var sp = result['adj_list']
-    //             var stock = result['ticker']
-    //             createGraph(data, ticker)
-    //             var close = $('.cg'+ticker)
-    //             console.log(close)
-    //             close.on('click', function(){
-    //                 graph.css('display','none')
-    //             })  
-    //         }
-    //     });
-
-    // });
-// ====================================================================================
-
-
-// ============================Portfolio Graph==========================================
+// ============================Graph==========================================
     
-    var createPortfolioGraph = function(sentiment, div){
+    var createGraph = function(sentiment, div){
         require.config({
             baseUrl: '/js',
             paths: {
@@ -90,23 +14,20 @@ $(document).ready(function(){
         console.log(sentiment)
         require(["d3", "c3"], function(d3, c3){
             var data = sentiment
-            console.log(typeof data)
             var date_with_title = data['date_list']
-            console.log(date_with_title)
             var dates_only = data['date_list']
             dates_only.shift()
-            var sp = data['stock']
-            console.log(sp)
-            var portfolio = sentiment['sentiment']
-            console.log(portfolio)
+            var stockReturns = data['stock']
+            var stockSentiment = sentiment['sentiment']
+            console.log(stockSentiment)
             var chart = c3.generate({
                 bindto: '#'+div.attr('id'),
                 data: {
                     x: date_with_title[0],
                     columns: [
                         date_with_title,
-                        sp,
-                        portfolio
+                        stockReturns,
+                        stockSentiment
                     ],
                     axes: {
                         NFLX:'y',
@@ -122,8 +43,10 @@ $(document).ready(function(){
                     },
                     y2: {
                         label: {
-                            text: 'second axis'
-                        }
+                            text: 'second axis',
+                            position:'inner-middle'
+                        },
+                        show: true
                         
                     },
                     x: {
@@ -137,21 +60,11 @@ $(document).ready(function(){
         });
     };
     
-    var portfolioGraph = $("#nflx_graph");
-    var port_data = $('#nflx_graph').data('returns');
-    createPortfolioGraph(port_data, $('#nflx_graph'))
-// ====================================================================================
+    var nflxDiv = $("#nflx_graph");
+    var nflxData = $('#nflx_graph').data('returns');
+    createGraph(nflxData, nflxDiv)
 
-// =================================Portfolio 3yr===============================================
-    
-//     var portfolioGraphThree = $("#portfolio_vs_sp_three");
-//     var port_data_three = $('#portfolio_vs_sp_three').data('returns');
-//     createPortfolioGraph(port_data_three, portfolioGraphThree)
-
-
-// // =================================Portfolio 5yr===============================================
-
-//     var portfolioGraphFive = $("#portfolio_vs_sp_five");
-//     var port_data_three = $('#portfolio_vs_sp_five').data('returns');
-//     createPortfolioGraph(port_data_three, portfolioGraphFive)
 });
+
+
+
